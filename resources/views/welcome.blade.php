166c2f42f1fdc425202e8115c399512beb8a83ba
Tiago de Castro Lim <casmei@protonmail.com>
2024-08-31 12:44:31 -0300

    remove(book): messagem explicando sobre as imagens

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,21 +14,6 @@
         </div>
 
         <div class="sm:pt-20 pt-32">
-            <div class="mb-5 p-4 text-gray-800 rounded-lg bg-gray-50 dark:bg-gray-800 dark:text-gray-300" role="alert">
-                <div class="flex items-center mb-3">
-                    <svg class="flex-shrink-0 inline w-4 h-4 me-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                        <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"/>
-                    </svg>
-                    <h2 class="text-lg font-bold">Olá, Entrevistador(a)!</h2>
-                </div>
-                <div>
-                    <span class="font-medium">Nota sobre os Livros: <strong>Imagens Ausentes</strong></span><br>
-                    Neste momento, os livros não têm imagens associadas.
-                    Isso é intencional, pois você pode usar suas credenciais do AWS S3 para adicionar imagens.<br>
-                    Aproveite a oportunidade para editar ou adicionar as imagens que desejar. 😁
-                </div>
-            </div>
-
             <div class="grid grid-cols-1 md:grid-cols-2 gap-6">
                 @foreach ($books as $book)
                     <x-book-card :book="$book"/>
